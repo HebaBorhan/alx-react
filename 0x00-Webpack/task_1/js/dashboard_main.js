52c1d0e08d236192c04d8f6727eabd48e2acb17b
HebaZaki <h_borhan@hotmail.com>
2024-08-23 03:47:51 +0200

    import jquery

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,17 +2,18 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 let count = 0;
+
 function updateCounter() {
     count++;
     $('#count').text(`${count} clicks on the button`);
 }
+
+const debouncedUpdateCounter = _.debounce(updateCounter, 300);
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
-
-const debouncedUpdateCounter = _.debounce(updateCounter, 300);
 
 $('button').on('click', debouncedUpdateCounter);
