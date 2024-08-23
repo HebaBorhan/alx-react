00b90e3a0a5566be97766f9735391539c2bff4d3
HebaZaki <h_borhan@hotmail.com>
2024-08-23 12:06:29 +0200

    update

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -9,43 +9,38 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|jpe?g|gif)$/i,
-            type: 'asset/resource',
-            use: [
-                {
-                    loader: 'image-webpack-loader',
-                    options: {
-                        mozjpeg: {
-                            progressive: true,
-                            quality: 65,
-                        },
-                        optipng: {
-                            enabled: true,
-                        },
-                        pngquant: {
-                            quality: [0.65, 0.90],
-                            speed: 4,
-                        },
-                        gifsicle: {
-                            interlaced: false,
-                        },
-                        webp: {
-                            quality: 75,
-                        },
-                    },
-                },
-            ],
-        },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
     ],
-},
-optimization: {
-    splitChunks: {
-        chunks: 'all',
-    },
-},
+  },
 };
