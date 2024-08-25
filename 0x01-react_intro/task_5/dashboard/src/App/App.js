0b45dba819ce8ea1d9ad2e87ce5ba9590b6c9a73
HebaZaki <h_borhan@hotmail.com>
2024-08-26 00:21:15 +0200

    app files

---
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import holbertonLogo from './holbertonLogo.png';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+
+          <button type="submit">OK</button>
+        </form>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
