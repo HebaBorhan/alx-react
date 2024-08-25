36d13676b15ec472b7c271f8f1e1728f81a64d0e
HebaZaki <h_borhan@hotmail.com>
2024-08-25 14:01:07 +0200

    tests

---
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('App renders without crashing', () => {
+  render(<App />);
+});
+
+test('App renders a div with the class App-header', () => {
+  render(<App />);
+  const headerDiv = screen.getByRole('banner');
+  expect(headerDiv).toHaveClass('App-header');
+});
+
+test('App renders a div with the class App-body', () => {
+  render(<App />);
+  const bodyDiv = screen.getByText(/Login to access the full dashboard/i).closest('div');
+  expect(bodyDiv).toHaveClass('App-body');
+});
+
+test('App renders a div with the class App-footer', () => {
+  render(<App />);
+  const footerDiv = screen.getByText(/Copyright/i).closest('div');
+  expect(footerDiv).toHaveClass('App-footer');
+});
+
