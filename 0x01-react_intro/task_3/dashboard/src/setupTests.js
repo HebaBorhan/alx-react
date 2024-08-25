05326e423cf6186271de7c71708d7afc018c7ca3
HebaZaki <h_borhan@hotmail.com>
2024-08-25 14:59:56 +0200

    Prepare for deployment to GitHub Pages

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -2,10 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import '@sinonjs/text-encoding';
 import '@testing-library/jest-dom';
 
 import { configure } from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
-import 'text-encoding';
 
 configure({ adapter: new Adapter() });
