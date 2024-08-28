70454d17473833d53e4829d2effab5faf50895e9
HebaZaki <h_borhan@hotmail.com>
2024-08-29 00:11:10 +0200

    continuing from react intro

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+test('App renders without crashing', () => {
+  shallow(<App />);
+});
+
+test('App renders a div with the class App-header', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App-header').exists()).toBe(true);
+});
+
+test('App renders a div with the class App-body', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App-body').exists()).toBe(true);
+});
+
+test('App renders a div with the class App-footer', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App-footer').exists()).toBe(true);
+});
