9e8a0675cbe6e570ad9dc86cfe620e9747406316
HebaZaki <h_borhan@hotmail.com>
2024-08-29 00:49:45 +0200

    add  notifications

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import Login from './Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="App-body">
-        <Login />
+    <>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <div className="App-body">
+          <Login />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
-
-export default App;
