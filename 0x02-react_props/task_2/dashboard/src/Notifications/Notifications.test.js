25eb3048a1a5a5e0cffbc5e0d704f392a85b9ffa
HebaZaki <h_borhan@hotmail.com>
2024-09-02 21:42:56 +0300

    NotificationsItem Update

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Notifications from './Notifications';
 import NotificationItem from './NotificationItem';
 
-describe('Notifications Component', () => {
-  it('renders without crashing', () => {
-    render(<Notifications />);
-  });
+describe('<Notifications />', () => {
+    it('Notifications component renders without crashing', () => {
+        render(<Notifications />);
+    });
 
-  it('renders NotificationItem components', () => {
-    const { container } = render(<Notifications />);
-    const notificationItems = container.querySelectorAll('li');
-    expect(notificationItems.length).toBe(3);
-  });
+    it('renders 3 NotificationItem components', () => {
+        render(<Notifications />);
+        const notificationItems = screen.getAllByRole('listitem');
+        expect(notificationItems.length).toBe(3);
+    });
 
-  it('renders the first NotificationItem with correct html', () => {
-    const { getByText } = render(<Notifications />);
-    const firstNotification = getByText('New course available');
-    expect(firstNotification).toBeTruthy();
-  });
+    it('renders correct text', () => {
+        render(<Notifications />);
+        const paragraphElement = screen.getByText('Here is the list of notifications');
+        expect(paragraphElement).toBeInTheDocument();
+    });
+
+    it('renders first NotificationItem properly', () => {
+        render(<Notifications />);
+        // Use a regex to match the text with any leading/trailing characters
+        const firstNotificationItem = screen.getByText(/New course available/i);
+        expect(firstNotificationItem).toBeInTheDocument();
+    });
 });
