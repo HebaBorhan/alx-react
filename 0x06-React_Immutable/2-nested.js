167d3a640e2c0b5c71b2a947449a9fad1e918d31
HebaZaki <h_borhan@hotmail.com>
2024-09-09 07:48:03 +0300

    return value of object

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,17 +1,3 @@
-import { Map } from 'immutable';
-
-export default function accessImmutableObject(object, path) {
-  let currentValue = object;
-  for (let i = 0; i < path.length; i++) {
-    const key = path[i];
-    if (currentValue instanceof Map) {
-      currentValue = currentValue.get(key);
-    } else {
-      currentValue = currentValue[key];
-    }
-    if (currentValue === undefined) {
-      return undefined;
-    }
-  }
-  return currentValue;
+export default function accessImmutableObject(object, array) {
+  return array.reduce((acc, key) => (acc && acc[key] !== undefined) ? acc[key] : undefined, object);
 }
