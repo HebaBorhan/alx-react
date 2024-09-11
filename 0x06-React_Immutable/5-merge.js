5ad0445eee2e881b31f41c5a7ee69a5e6dfb554a
HebaZaki <h_borhan@hotmail.com>
2024-09-11 23:52:04 +0300

    Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -5,3 +5,16 @@ export function concatElements(page1, page2) {
   // Convert both arrays into Immutable Lists & concatenate them
   return List(page1).concat(List(page2));
 }
+
+// Merge 2 objects into an Immutable List
+export function mergeElements(page1, page2) {
+  // Convert both objects into Immutable Maps
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+
+  // Merge Maps
+  const mergedMap = map1.mergeWith((oldValue, newValue) => newValue, map2);
+
+  // Convert merged Map values into an Immutable List
+  return List(mergedMap.values());
+}
