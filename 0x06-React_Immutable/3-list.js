5c5f4bcb9d9cabe7fde478a602d50f0945be2ae4
HebaZaki <h_borhan@hotmail.com>
2024-09-11 23:21:00 +0300

    list

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,11 +1,11 @@
 import { List } from 'immutable';
 
 // Convert array to an immutable List
-export function getListObject (array) {
+export function getListObject(array) {
   return List(array);
 }
 
 // Add an element to an immutable List
-export function addElementToList (list, element) {
+export function addElementToList(list, element) {
   return list.push(element);
 }
