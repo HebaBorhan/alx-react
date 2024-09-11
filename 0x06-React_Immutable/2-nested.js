185789b55fd2569439a0027c0837f932c92a116e
HebaZaki <h_borhan@hotmail.com>
2024-09-11 22:59:07 +0300

    return value of object

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,3 +1,3 @@
-export default function accessImmutableObject(object, array) {
+export default function accessImmutableObject (object, array) {
   return array.reduce((acc, key) => (acc && acc[key] !== undefined) ? acc[key] : undefined, object);
 }
