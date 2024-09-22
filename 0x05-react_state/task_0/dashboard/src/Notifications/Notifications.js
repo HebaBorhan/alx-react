ada17e0272b84d7d4abfbc09562f0ac2e7a435fc
HebaZaki <h_borhan@hotmail.com>
2024-09-22 11:49:13 +0300

    update Notifications

--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -1,42 +1,111 @@
-// Existing imports...
 import React from 'react';
-import { mount, shallow } from 'enzyme';
-import App from './App';
-import Notifications from '../Notifications/Notifications';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
-import CourseList from '../CourseList/CourseList';
-import { StyleSheetTestUtils } from 'aphrodite';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
+import { StyleSheet, css } from 'aphrodite';
 
-StyleSheetTestUtils.suppressStyleInjection();
-
-describe('<App />', () => {
-  // Existing tests...
-
-  it('default displayDrawer state is false', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.state('displayDrawer')).toBe(false);
-  });
-
-  it('displayDrawer is true after calling handleDisplayDrawer', () => {
-    const wrapper = shallow(<App />);
-    wrapper.instance().handleDisplayDrawer();
-    expect(wrapper.state('displayDrawer')).toBe(true);
-  });
-
-  it('displayDrawer is false after calling handleHideDrawer', () => {
-    const wrapper = shallow(<App />);
-    wrapper.instance().handleDisplayDrawer();
-    wrapper.instance().handleHideDrawer();
-    expect(wrapper.state('displayDrawer')).toBe(false);
-  });
+const styles = StyleSheet.create({
+  // Existing styles...
+  hiddenMenuItem: {
+    display: "none",
+  },
+  menuItem: {
+    position: "fixed",
+    right: "20px",
+    top: "20px",
+    backgroundColor: '#fff8f8',
+    ':hover': {
+      cursor: 'pointer',
+    },
+  },
+  notifications: {
+    // Existing styles...
+  },
+  ul: {
+    // Existing styles...
+  },
 });
 
-describe('when isLoggedIn is true', () => {
-  // Existing tests...
-});
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
 
-describe("Test <App /> with logOut function", () => {
-  // Existing tests...
-});
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  shouldComponentUpdate(newProps) {
+    return newProps.listNotifications.length > this.props.listNotifications.length ||
+           newProps.displayDrawer !== this.props.displayDrawer;
+  }
+
+  render() {
+    return (
+      <>
+        <div
+          className={css(styles.menuItem, this.props.displayDrawer ? styles.hiddenMenuItem : '')}
+          onClick={this.props.handleDisplayDrawer} // Calls handleDisplayDrawer
+        >
+          <p>Your notifications</p>
+        </div>
+        {this.props.displayDrawer &&
+          <div className={css(styles.notifications)}>
+            <ul className={css(styles.ul)}>
+              {this.props.listNotifications.length === 0 ? (
+                <NotificationItem type='default' value="No new notification for now" />
+              ) : (
+                <>
+                  <p>Here is the list of notifications</p>
+                  {this.props.listNotifications.map((notification) => (
+                    <NotificationItem
+                      key={notification.id}
+                      type={notification.type}
+                      value={notification.value}
+                      html={notification.html}
+                      markAsRead={() => this.markAsRead(notification.id)}
+                    />
+                  ))}
+                </>
+              )}
+            </ul>
+            <button
+              style={{
+                marginRight: '10px',
+                backgroundColor: 'white',
+                border: '1px solid lightgrey',
+                borderRadius: '5px',
+                padding: '3px',
+                position: 'absolute',
+                right: '30px',
+                top: '15px',
+              }}
+              aria-label='close'
+              onClick={this.props.handleHideDrawer} // Calls handleHideDrawer
+            >
+              <img width="10px" src={closeIcon} alt="close button"></img>
+            </button>
+          </div>
+        }
+      </>
+    );
+  }
+}
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {},
+};
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  handleDisplayDrawer: PropTypes.func,  // Added PropType validation
+  handleHideDrawer: PropTypes.func,  // Added PropType validation
+};
+
+export default Notifications;
