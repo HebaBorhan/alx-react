4be1476a776a6a5ad8c579c38c969ba37a1efadb
HebaZaki <h_borhan@hotmail.com>
2024-09-22 15:44:32 +0300

    context

---
+++ 0x05-react_state/task_2/dashboard/src/App/AppContext.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const defaultUser = {
+  email: '',
+  password: '',
+  isLoggedIn: false,
+};
+
+const AppContext = React.createContext({
+  user: defaultUser,
+  logOut: () => {},
+});
+
+export default AppContext;
