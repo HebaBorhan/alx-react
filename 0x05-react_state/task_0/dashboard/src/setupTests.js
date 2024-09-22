dbfd2322340497d158b4e714a82b7ff8a56f2150
HebaZaki <h_borhan@hotmail.com>
2024-09-22 13:18:31 +0300

    update

--- 0x05-react_state/task_0/dashboard/src/setupTests.js
+++ 0x05-react_state/task_0/dashboard/src/setupTests.js
@@ -1,5 +1,2 @@
-// setup file
-import { configure } from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
-
-configure({ adapter: new Adapter() });
+// setupTests.js
+import '@testing-library/jest-dom/extend-expect';
