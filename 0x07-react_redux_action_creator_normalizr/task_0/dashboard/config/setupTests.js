05bb0b2453ff60835718763df2ae405923f03d25
HebaZaki <h_borhan@hotmail.com>
2024-09-25 16:24:12 +0300

    update

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/config/setupTests.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/config/setupTests.js
@@ -1,2 +1,9 @@
 // setupTests.js
 import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('aphrodite', () => ({
+    StyleSheet: {
+      create: () => ({}),
+    },
+    css: () => '',
+  }));
