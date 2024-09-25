f2632603409d31b2136a82f4038fb44a5f8d5fc9
HebaZaki <h_borhan@hotmail.com>
2024-09-25 20:50:34 +0300

    ui action creator

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -1,24 +1,26 @@
-import {
-    LOGIN,
-    LOGOUT,
-    DISPLAY_NOTIFICATION_DRAWER,
-    HIDE_NOTIFICATION_DRAWER
-  } from './uiActionTypes';
-  
-  export const login = (email, password) => ({
-    type: LOGIN,
-    user: { email, password }
-  });
-  
-  export const logout = () => ({
-    type: LOGOUT
-  });
-  
-  export const displayNotificationDrawer = () => ({
-    type: DISPLAY_NOTIFICATION_DRAWER
-  });
-  
-  export const hideNotificationDrawer = () => ({
-    type: HIDE_NOTIFICATION_DRAWER
-  });
-  
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from './uiActionTypes';
+import { bindActionCreators } from 'redux';
+
+export const login = (email, password) => ({
+  type: LOGIN,
+  user: { email, password },
+});
+
+export const logout = () => ({
+  type: LOGOUT,
+});
+
+export const displayNotificationDrawer = () => ({
+  type: DISPLAY_NOTIFICATION_DRAWER,
+});
+
+export const hideNotificationDrawer = () => ({
+  type: HIDE_NOTIFICATION_DRAWER,
+});
+
+// Binding action creators
+export const bindUIActions = (dispatch) =>
+  bindActionCreators(
+    { login, logout, displayNotificationDrawer, hideNotificationDrawer },
+    dispatch
+  );
