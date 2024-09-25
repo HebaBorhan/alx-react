e9b3dbe872aea1adc306f31c5271b75617f6753f
HebaZaki <h_borhan@hotmail.com>
2024-09-25 17:05:20 +0300

    notifications schema

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,10 +1,20 @@
 import * as notificationsData from '../../notifications.json';
+import { normalize, schema } from 'normalizr';
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+const normalizedData = normalize(notificationsData, [notification]);
+
+export const getNormalizedData = () => normalizedData;
 
 export default function getAllNotificationsByUser(userId) {
-    const { default: notifications } = notificationsData;
+  const { notifications } = normalizedData.entities;
 
-    // Only return the context part of the notifications
-    return notifications
-        .filter((notification) => notification.author.id === userId)
-        .map((notification) => notification.context);
+  return Object.keys(notifications)
+    .filter((id) => notifications[id].author === userId)
+    .map((id) => notifications[id].context);
 }
