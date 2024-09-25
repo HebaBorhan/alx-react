05cbe4d7515731d6dbfd4c20dacf29d554e57b71
HebaZaki <h_borhan@hotmail.com>
2024-09-25 16:19:15 +0300

    notifications schema

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,11 +1,10 @@
 import * as notificationsData from '../../notifications.json';
 
-/**
- * Get all notifications by specific userId
- * @param {string} userId - id of the author (user)
- * @returns {Array} - Array of notifications where author id matches userId.
- */
 export default function getAllNotificationsByUser(userId) {
-  // filter notifications by userId
-  return notificationsData.default.filter((notification) => notification.author.id === userId);
+    const { default: notifications } = notificationsData;
+
+    // Only return the context part of the notifications
+    return notifications
+        .filter((notification) => notification.author.id === userId)
+        .map((notification) => notification.context);
 }
