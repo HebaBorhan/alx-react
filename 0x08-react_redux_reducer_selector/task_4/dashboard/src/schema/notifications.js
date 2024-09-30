cc4af43ed98c48fcae0e4b0cbe24e80fb604c994
HebaZaki <h_borhan@hotmail.com>
2024-09-30 22:23:14 +0300

    use Immutable.js and Normalizr

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
@@ -1,32 +1,23 @@
-import * as notificationsData from '../../notifications.json';
 import { normalize, schema } from 'normalizr';
+import * as notificationObjects from '../../notifications.json';
 
 // Define schemas
-const user = new schema.Entity('users');
+const user = new schema.Entity("users");
 const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
-const notification = new schema.Entity('notifications', {
-  author: user,
-  context: message,
-});
+const notification = new schema.Entity('notifications', { author: user, context: message });
 
-// Normalize the data
-const normalizedData = normalize(notificationsData, [notification]);
+// Normalize notificationObjects for default data
+const normalizedData = normalize(notificationObjects.default, [notification]);
 
-// Export normalized data for use elsewhere
-export const getNormalizedData = () => normalizedData;
+// Normalize passed data (used in reducers)
+export const notificationsNormalizer = (data) => normalize(data, [notification]);
 
-/**
- * Retrieves all notifications by a specific userId
- * @param {string} userId - The ID of the user whose notifications are needed
- * @returns {Array} - Array of message objects associated with the user's notifications
- */
-export default function getAllNotificationsByUser(userId) {
+// Function to get notifications by user
+export const getAllNotificationsByUser = (userId) => {
   const { notifications, messages } = normalizedData.entities;
-  
-  // Filter notifications by the userId and map them to the corresponding messages
-  const userNotifications = Object.values(notifications).filter(
-    (notification) => notification.author === userId
-  ).map((notification) => messages[notification.context]);
+  return Object.values(notifications)
+    .filter(notification => notification.author === userId)
+    .map(notification => messages[notification.context]);
+};
 
-  return userNotifications;
-}
+export { normalizedData };
