d07ce78ce64416fcc961016940948d3cefa35d2a
HebaZaki <h_borhan@hotmail.com>
2024-09-30 22:23:51 +0300

    use Immutable.js and Normalizr

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,27 +1,46 @@
-import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from '../actions/notificationActionTypes';
-import { notificationsNormalizer } from '../schema/notifications';
-import { Map } from 'immutable';
-import { NotificationTypeFilters } from '../actions/notificationActionTypes';
-
-const initialState = Map({
-  notifications: Map(),
-  filter: NotificationTypeFilters.DEFAULT,
-});
-
-// Reducer function
-export default function notificationReducer(state = initialState, action = {}) {
-  switch (action.type) {
-    case FETCH_NOTIFICATIONS_SUCCESS:
-      const normalizedData = notificationsNormalizer(action.data);
-      return state.mergeIn(['notifications'], normalizedData.entities.notifications);
-
-    case MARK_AS_READ:
-      return state.setIn(['notifications', String(action.index), 'isRead'], true);
-
-    case SET_TYPE_FILTER:
-      return state.set('filter', action.filter);
-
-    default:
-      return state;
-  }
-}
+import {
+    FETCH_NOTIFICATIONS_SUCCESS,
+    MARK_AS_READ,
+    SET_TYPE_FILTER,
+    NotificationTypeFilters,
+  } from '../actions/notificationActionTypes';
+  import { fromJS } from 'immutable';
+  import { notificationsNormalizer } from '../schema/notifications';
+  
+  const initialState = fromJS({
+    notifications: {},
+    filter: NotificationTypeFilters.DEFAULT,
+  });
+  
+  const notificationReducer = (state = initialState, action = {}) => {
+    switch (action.type) {
+      case FETCH_NOTIFICATIONS_SUCCESS: {
+        // Ensure that notifications have the `isRead` field set to false
+        const notificationList = action.data.map(notification => ({
+          ...notification,
+          isRead: false,
+        }));
+        const normalizedData = notificationsNormalizer(notificationList);
+        return state.mergeDeep({
+          notifications: fromJS(normalizedData.entities.notifications),
+        });
+      }
+  
+      case MARK_AS_READ: {
+        return state.setIn(
+          ['notifications', String(action.index), 'isRead'],
+          true
+        );
+      }
+  
+      case SET_TYPE_FILTER: {
+        return state.set('filter', action.filter);
+      }
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default notificationReducer;
+  
