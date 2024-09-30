31e6f5b30a091414881acfeb27a074e9894187fe
HebaZaki <h_borhan@hotmail.com>
2024-09-30 22:24:42 +0300

    use Immutable.js and Normalizr

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.js
@@ -1,23 +1,31 @@
+import { fromJS } from 'immutable';
 import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActionTypes';
 import { coursesNormalizer } from '../schema/courses';
-import { Map } from 'immutable';
 
-const initialState = Map({
-  courses: Map(),
+const initialState = fromJS({
+  courses: {},
 });
 
-// Reducer function
-export default function courseReducer(state = initialState, action = {}) {
+export default function courseReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_COURSE_SUCCESS:
-      const normalizedData = coursesNormalizer(action.data);
-      return state.mergeIn(['courses'], normalizedData.entities.courses);
+      const normalizedCourses = coursesNormalizer(action.data).entities.courses;
+      
+      // Add the `isSelected` field to each course
+      const updatedCourses = Object.keys(normalizedCourses).reduce((acc, key) => {
+        acc[key] = { ...normalizedCourses[key], isSelected: false };
+        return acc;
+      }, {});
+
+      return state.merge({
+        courses: fromJS(updatedCourses),
+      });
 
     case SELECT_COURSE:
-      return state.setIn(['courses', String(action.index), 'isSelected'], true);
+      return state.setIn(['courses', action.index, 'isSelected'], true);
 
     case UNSELECT_COURSE:
-      return state.setIn(['courses', String(action.index), 'isSelected'], false);
+      return state.setIn(['courses', action.index, 'isSelected'], false);
 
     default:
       return state;
