5cbb61bd5e5fa8405fe63be0b2935c6fe420642e
HebaZaki <h_borhan@hotmail.com>
2024-09-30 22:24:26 +0300

    update

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.test.js
@@ -1,7 +1,13 @@
-import courseReducer, { initialState } from './courseReducer';
-import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActionTypes';
 import { fromJS } from 'immutable';
+import courseReducer from '../reducers/courseReducer';
+import { FETCH_COURSE_SUCCESS } from '../actions/courseActionTypes';
 
+// Mock the initial state using Immutable.js
+const initialState = fromJS({
+  courses: {},
+});
+
+// Test the course reducer
 describe('courseReducer', () => {
   it('should return the initial state when no action is passed', () => {
     const state = courseReducer(undefined, {});
@@ -12,45 +18,21 @@ describe('courseReducer', () => {
     const action = {
       type: FETCH_COURSE_SUCCESS,
       data: [
-        { id: 1, name: "ES6", credit: 60 },
-        { id: 2, name: "Webpack", credit: 20 },
-        { id: 3, name: "React", credit: 40 }
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 },
       ],
     };
-    const state = courseReducer(initialState, action);
 
-    expect(state.toJS()).toEqual({
+    const expectedState = fromJS({
       courses: {
-        1: { id: 1, name: "ES6", credit: 60, isSelected: false },
-        2: { id: 2, name: "Webpack", credit: 20, isSelected: false },
-        3: { id: 3, name: "React", credit: 40, isSelected: false }
-      }
+        1: { id: 1, name: 'ES6', credit: 60, isSelected: false },
+        2: { id: 2, name: 'Webpack', credit: 20, isSelected: false },
+        3: { id: 3, name: 'React', credit: 40, isSelected: false },
+      },
     });
-  });
 
-  it('should handle SELECT_COURSE and mark the course as selected', () => {
-    const action = { type: SELECT_COURSE, index: 2 };
-    const state = courseReducer(fromJS({
-      courses: {
-        1: { id: 1, isSelected: false },
-        2: { id: 2, isSelected: false },
-        3: { id: 3, isSelected: false }
-      }
-    }), action);
-
-    expect(state.toJS().courses[2].isSelected).toEqual(true);
-  });
-
-  it('should handle UNSELECT_COURSE and mark the course as unselected', () => {
-    const action = { type: UNSELECT_COURSE, index: 2 };
-    const state = courseReducer(fromJS({
-      courses: {
-        1: { id: 1, isSelected: false },
-        2: { id: 2, isSelected: true },
-        3: { id: 3, isSelected: false }
-      }
-    }), action);
-
-    expect(state.toJS().courses[2].isSelected).toEqual(false);
+    const state = courseReducer(initialState, action);
+    expect(state.toJS()).toEqual(expectedState.toJS());
   });
 });
